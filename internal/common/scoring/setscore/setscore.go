d7bd458e69c2cb9cf807d535cbc2552aee8f8fa3
Ciro Bispo <cirobispo@zoho.com>
2026-03-06 11:48:47 -0300

    Evolved setScore and add tests.

--- internal/common/scoring/setscore/setscore.go
+++ internal/common/scoring/setscore/setscore.go
@@ -1,38 +1,66 @@
 package setscore
 
 import (
-	"math"
-
-	"github.com/cirobispo/sandbox/internal/common/gaming/game"
 	"github.com/cirobispo/sandbox/internal/common/scoring"
 	"github.com/cirobispo/sandbox/internal/common/turning"
 )
 
 type OnSetScore func(scoreA, scoreB int, isTieBreak, done bool)
+type ParamOption func(score *SetScore)
 
 type SetScore struct {
-	startSide      turning.SideTurn
-	setSize        int
-	decidingSet    bool
-	scoreA, scoreB int
+	sideToBegin      turning.SideTurn
+	maxEven          int
+	confirmationSize int
+	scoreA, scoreB   int
 
 	onAfterScoreEvent []OnSetScore
 }
 
-func New(startSide turning.SideTurn, decidingSet bool) SetScore {
-	return SetScore{
-		startSide:         startSide,
-		setSize:           6,
-		decidingSet:       decidingSet,
+func WithDefaultSet(sideToBegin turning.SideTurn) ParamOption {
+	return func(score *SetScore) {
+		score.sideToBegin = sideToBegin
+		score.maxEven = 6
+		score.confirmationSize = 2
+	}
+}
+
+func WithSideSizeAndTieBreak(sideToBegin turning.SideTurn, size int, decidingGame, tieBreakForLastEven bool) ParamOption {
+	return func(score *SetScore) {
+		score.sideToBegin = sideToBegin
+		score.maxEven = size
+		score.confirmationSize = 2
+		if decidingGame {
+			score.confirmationSize--
+		}
+	}
+}
+
+func New(param ParamOption) SetScore {
+	result := SetScore{
 		scoreA:            0,
 		scoreB:            0,
 		onAfterScoreEvent: make([]OnSetScore, 0),
+	}
+
+	if param != nil {
+		param(&result)
+	}
+
+	return result
+}
+
+func (ss SetScore) executeOnAfterScoreEvent(scoreA, scoreB int) {
+	done, isTieBreak := ss.Done(), ss.IsTieBreak()
+	for i := range ss.onAfterScoreEvent {
+		event := ss.onAfterScoreEvent[i]
+		event(scoreA, scoreB, isTieBreak, done)
 	}
 }
 
 func (ss *SetScore) getScores() (*int, *int) {
 	sA, sB := &ss.scoreA, &ss.scoreB
-	if ss.startSide == turning.STB {
+	if ss.sideToBegin == turning.STB {
 		sA, sB = &ss.scoreB, &ss.scoreA
 	}
 
@@ -43,25 +71,15 @@ func (ss *SetScore) AddOnAfterScoreEvent(event OnSetScore) {
 	ss.onAfterScoreEvent = append(ss.onAfterScoreEvent, event)
 }
 
-func (ss SetScore) executeOnAfterScoreEvent(scoreA, scoreB int) {
-	done, isTieBreak := ss.Done(), ss.isTieBreak()
-	for i := range ss.onAfterScoreEvent {
-		event := ss.onAfterScoreEvent[i]
-		event(scoreA, scoreB, isTieBreak, done)
-	}
-}
-
-// func (ss SetScore)
-
-func (ss *SetScore) AddGame(g *game.Game) {
-	if ss.Done() || !g.Score().Done() { // am I acepting more points?
+func (ss *SetScore) AddGameScore(score scoring.Scoring) {
+	if ss.Done() || !score.Done() { // am I acepting more points?
 		return
 	}
 
 	sA, sB := ss.getScores()
 	sideToAdd := sA
 
-	if g.Score().Side() == scoring.SSB {
+	if score.Side() == scoring.SSB {
 		sideToAdd = sB
 	}
 
@@ -76,14 +94,18 @@ func (ss SetScore) Result() (int, int) {
 
 func (ss SetScore) Done() bool {
 	sA, sB := ss.Result()
-	result := (ss.decidingSet && (sA >= ss.setSize || sB >= ss.setSize)) ||
-		(!ss.decidingSet && (sA >= ss.setSize || sB >= ss.setSize) && (math.Abs(float64(sA-sB)) > 1))
+
+	sideAWins := (sA >= ss.maxEven && sA-sB >= ss.confirmationSize)
+	sideBWins := (sB >= ss.maxEven && sB-sA >= ss.confirmationSize)
+	result := sideAWins || sideBWins
+
 	return result
 }
 
-func (ss SetScore) isTieBreak() bool {
+func (ss SetScore) IsTieBreak() bool {
 	sA, sB := ss.Result()
-	result := (sA == ss.setSize && sB == ss.setSize)
+
+	result := (sA >= ss.maxEven && sB >= ss.maxEven)
 
 	return result
 }
---
+++ internal/common/scoring/setscore/setscore_test.go
@@ -0,0 +1,185 @@
+package setscore
+
+import (
+	"testing"
+
+	"github.com/cirobispo/sandbox/internal/common/scoring"
+	"github.com/cirobispo/sandbox/internal/common/turning"
+)
+
+type score struct {
+	scoreA, scoreB int
+}
+
+func (s score) Done() bool {
+	return true
+}
+
+func (s score) Result() (int, int) {
+	return s.scoreA, s.scoreB
+}
+
+func (s score) Side() scoring.ScoringSide {
+	if s.scoreB > s.scoreA {
+		return scoring.SSB
+	}
+
+	return scoring.SSA
+}
+
+func runTest(custom bool, results []scoring.Scoring, SideA, SideB int, t *testing.T) {
+	score := New(WithDefaultSet(turning.STA))
+	if custom {
+		if SideB > SideA {
+			score = New(WithSideSizeAndTieBreak(turning.STA, SideB, true, true))
+		} else {
+			score = New(WithSideSizeAndTieBreak(turning.STA, SideA, true, true))
+		}
+	}
+
+	score.AddOnAfterScoreEvent(func(scoreA, scoreB int, isTieBreak, done bool) {
+		if isTieBreak || done {
+			t.Logf("Score (%d x %d)\n", scoreA, scoreB)
+		}
+	})
+
+	t.Logf("Testing a result for a Set with %d games. Expected result (%d x %d)\n", len(results), SideA, SideB)
+	for j := range results {
+		item := results[j]
+		score.AddGameScore(item)
+	}
+
+	sA, sB := score.Result()
+	if sA != SideA || sB != SideB {
+		for j := range results {
+			scoreA, scoreB := scoring.Score2GameText(results[j].Result())
+			t.Logf("Score (%v x %v)\n", scoreA, scoreB)
+		}
+		t.Errorf("\n\nSet should be (%d x %d) not (%d x %d)\n", SideA, SideB, sA, sB)
+	}
+}
+
+func Test_Set_6x4(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 2}, score{scoreA: 3, scoreB: 5},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 0, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 4, scoreB: 0},
+	}
+
+	runTest(false, scores, 6, 4, t)
+}
+
+func Test_Set_4x6(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 2}, score{scoreA: 3, scoreB: 5},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 0, scoreB: 4},
+
+		score{scoreA: 0, scoreB: 4}, score{scoreA: 1, scoreB: 4},
+	}
+
+	runTest(false, scores, 4, 6, t)
+}
+
+func Test_Set_6x0(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 4, scoreB: 1},
+
+		score{scoreA: 4, scoreB: 2}, score{scoreA: 4, scoreB: 0},
+
+		score{scoreA: 5, scoreB: 3}, score{scoreA: 4, scoreB: 0},
+	}
+
+	runTest(false, scores, 6, 0, t)
+}
+
+func Test_Set_0x6(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreB: 4, scoreA: 0}, score{scoreB: 4, scoreA: 1},
+
+		score{scoreB: 4, scoreA: 2}, score{scoreB: 4, scoreA: 0},
+
+		score{scoreB: 5, scoreA: 3}, score{scoreB: 4, scoreA: 0},
+	}
+
+	runTest(false, scores, 0, 6, t)
+}
+
+func Test_Set_7x5(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 2}, score{scoreA: 3, scoreB: 5},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 0, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, // score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, // score{scoreA: 1, scoreB: 4},
+	}
+
+	runTest(false, scores, 7, 5, t)
+}
+
+func Test_Set_4x3(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 2}, score{scoreA: 3, scoreB: 5},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, // score{scoreA: 0, scoreB: 4},
+	}
+
+	runTest(true, scores, 4, 3, t)
+}
+
+func Test_Set_3x4(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 2}, score{scoreA: 3, scoreB: 5},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		// score{scoreA: 4, scoreB: 0},
+		score{scoreA: 0, scoreB: 4},
+	}
+
+	runTest(true, scores, 3, 4, t)
+}
+
+func Test_Set_6x4_TooManyGames(t *testing.T) {
+	scores := []scoring.Scoring{
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 2}, score{scoreA: 3, scoreB: 5},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 1, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 0, scoreB: 4},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 4, scoreB: 0},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 4, scoreB: 0},
+
+		score{scoreA: 4, scoreB: 0}, score{scoreA: 4, scoreB: 0},
+	}
+
+	runTest(false, scores, 6, 4, t)
+}
