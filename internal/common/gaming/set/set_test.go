0bc9974f71e91b1100c58a50aaa2cb1e65ad5db1
Ciro Bispo <cirobispo@zoho.com>
2026-04-29 13:41:20 -0300

    tradução do set_test.go. ainda não todo traduzido. farei depois.

--- internal/common/gaming/set/set_test.go
+++ internal/common/gaming/set/set_test.go
@@ -9,34 +9,34 @@ import (
 	"github.com/cirobispo/sandbox/internal/common/turning/turn"
 )
 
-type testItem struct {
-	servingSide turning.TurningSide
-	score       scoring.EstadoResultadoEParametroPlacar
-	points      []point.Point
+type set struct {
+	ladoDoServico turning.TurningSide
+	placar        scoring.EstadoResultadoEParametroPlacar
+	pontos        []point.Point
 }
 
-func (t testItem) ServingSide() turning.TurningSide {
-	return t.servingSide
+func (s set) ServingSide() turning.TurningSide {
+	return s.ladoDoServico
 }
 
-func (t testItem) Score() scoring.EstadoResultadoEParametroPlacar {
-	return t.score
+func (s set) Score() scoring.EstadoResultadoEParametroPlacar {
+	return s.placar
 }
 
-func (t testItem) Points() []point.Point {
-	return t.points
+func (s set) Points() []point.Point {
+	return s.pontos
 }
 
-type score struct {
-	servingSide    turning.TurningSide
-	decidingPoint  bool
-	scoreA, scoreB int
+type placar struct {
+	ladoDoServico    turning.TurningSide
+	pontoDecisivo    bool
+	placarA, placarB int
 }
 
-func (s score) Terminado() bool {
-	sA, sB := s.Resultado()
+func (p placar) Terminado() bool {
+	sA, sB := p.Resultado()
 	diff := 2
-	if s.decidingPoint {
+	if p.pontoDecisivo {
 		diff--
 	}
 
@@ -48,42 +48,42 @@ func (s score) Terminado() bool {
 	return result
 }
 
-func (s score) Resultado() (int, int) {
-	return s.scoreA, s.scoreB
+func (p placar) Resultado() (int, int) {
+	return p.placarA, p.placarB
 }
 
-func (s score) Lado() scoring.LadoDoPlacar {
-	if s.scoreB > s.scoreA {
+func (p placar) Lado() scoring.LadoDoPlacar {
+	if p.placarB > p.placarA {
 		return scoring.LPOposto
 	}
 
 	return scoring.LPServico
 }
 
-func (s score) Tipo() scoring.TipoDoPlacar {
+func (p placar) Tipo() scoring.TipoDoPlacar {
 	return scoring.TPJogo
 }
 
-func (i *testItem) setServingSide(side turning.TurningSide) {
-	a, b := i.score.Resultado()
-	i.servingSide = side
-	i.score = newScore(side, a, b)
+func (s *set) setServingSide(lado turning.TurningSide) {
+	a, b := s.placar.Resultado()
+	s.ladoDoServico = lado
+	s.placar = novoPlacar(lado, a, b)
 }
 
-func newItem(scoreA, scoreB int) testItem {
-	result := testItem{score: newScore(turning.TSA, scoreA, scoreB), points: make([]point.Point, 0)}
+func novoSet(placarA, placarB int) set {
+	result := set{placar: novoPlacar(turning.TSA, placarA, placarB), pontos: make([]point.Point, 0)}
 	return result
 }
 
-func newScore(servingSide turning.TurningSide, scoreA, scoreB int) score {
-	return score{
-		servingSide: servingSide,
-		scoreA:      scoreA,
-		scoreB:      scoreB,
+func novoPlacar(servingSide turning.TurningSide, scoreA, scoreB int) placar {
+	return placar{
+		ladoDoServico: servingSide,
+		placarA:       scoreA,
+		placarB:       scoreB,
 	}
 }
 
-func runTest(blocks []testItem, SideA, SideB int, t *testing.T) {
+func runTest(blocks []set, SideA, SideB int, t *testing.T) {
 	myTurn := turn.New(turn.WithTurningSide(turning.TSA))
 	mySet := New(WithDefaultSet(myTurn))
 
@@ -93,20 +93,20 @@ func runTest(blocks []testItem, SideA, SideB int, t *testing.T) {
 			t.Log("FINAL ")
 		}
 
-		t.Logf("%s -> Score (%d x %d)\n", sideToServe, scoreA, scoreB)
+		t.Logf("%s -> Placar (%d x %d)\n", sideToServe, scoreA, scoreB)
 	})
 
 	mySet.AddOnPlayerChangeEvent(func() {
-		t.Logf("Players change side\n")
+		t.Logf("Jogadores mudam de lado\n")
 	})
 
 	for j := range blocks {
 		currentGame := mySet.NewGame()
 		item := blocks[j]
 		item.setServingSide(currentGame.ServingSide())
-		sideToServe = item.servingSide
+		sideToServe = item.ladoDoServico
 		if err := mySet.AddGame(item); err != nil {
-			t.Errorf("Error ao adicionar %v. Mensagem: %s", item, err)
+			t.Errorf("Erro ao adicionar %v. Mensagem: %s", item, err)
 		}
 	}
 	sA, sB := mySet.Score().Resultado()
@@ -114,32 +114,32 @@ func runTest(blocks []testItem, SideA, SideB int, t *testing.T) {
 		for i := range blocks {
 			item := blocks[i]
 			a, b := item.Score().Resultado()
-			t.Logf("Score game #%d (%d x %d)\n", i+1, a, b)
+			t.Logf("Placar do jogo #%d (%d x %d)\n", i+1, a, b)
 		}
-		t.Errorf("\n\nSet should be (%d x %d) not (%d x %d)\n", SideA, SideB, sA, sB)
+		t.Errorf("\n\nSet deveria ser (%d x %d) não (%d x %d)\n", SideA, SideB, sA, sB)
 	}
 }
 
 func Test_6x4(t *testing.T) {
-	data := []testItem{
-		newItem(6, 4), newItem(2, 6),
-		newItem(6, 3), newItem(1, 6),
-		newItem(6, 4), newItem(4, 6),
-		newItem(6, 4), newItem(4, 6),
-		newItem(6, 4), newItem(6, 4),
+	data := []set{
+		novoSet(6, 4), novoSet(2, 6),
+		novoSet(6, 3), novoSet(1, 6),
+		novoSet(6, 4), novoSet(4, 6),
+		novoSet(6, 4), novoSet(4, 6),
+		novoSet(6, 4), novoSet(6, 4),
 		// newItem(6, 4), newItem(6, 4),
 	}
 	runTest(data, 6, 4, t)
 }
 
 func Test_4x6(t *testing.T) {
-	data := []testItem{
-		newItem(6, 4), newItem(2, 6),
-		newItem(6, 3), newItem(1, 6),
-		newItem(6, 4), newItem(4, 6),
-		newItem(6, 4), newItem(4, 6),
-		newItem(5, 7), newItem(6, 6),
-		// newItem(6, 4), newItem(6, 4),
+	data := []set{
+		novoSet(6, 4), novoSet(2, 6),
+		novoSet(6, 3), novoSet(1, 6),
+		novoSet(6, 4), novoSet(4, 6),
+		novoSet(6, 4), novoSet(5, 7),
+		novoSet(5, 7), novoSet(4, 6),
+		// newItem(4, 6), newItem(4, 6),
 	}
 	runTest(data, 4, 6, t)
 }
