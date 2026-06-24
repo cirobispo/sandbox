d93ce632e964c1fdd2b760756433759b16bff24f
Ciro Bispo <cirobispo@zoho.com>
2026-06-23 23:24:23 -0300

    tradução.

--- internal/comum/jogos/jogos.go
+++ internal/comum/jogos/jogos.go
@@ -1,6 +1,6 @@
 package jogos
 
 type AoAdicionarPonto func(placarA, placarB int, terminado bool)
-type OnAfterAddingGame func(scoreA, scoreB int, done bool)
+type OnAfterAddingGame func(placarA, placarB int, terminado bool)
 type AoAdicionarSet func(placarA, placarB int, terminado bool)
 type OnPlayerChangeSide func()
