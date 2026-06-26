71187023b998a2f4d8bb9611747050575918f569
Ciro Bispo <cirobispo@zoho.com>
2026-06-25 22:39:40 -0300

    apenas deixando as funções vivas. antes estavam comentadas.

--- internal/comum/estatisticas/estatisticas.go
+++ internal/comum/estatisticas/estatisticas.go
@@ -1,36 +1,38 @@
 package estatisticas
 
+import "github.com/cirobispo/sandbox/internal/comum/golpes"
+
 type predicate func(indice int) bool
 
-// func contar(hits []TipoAcaoGolpe, f predicate) int {
-// 	tamanho := len(hits)
-// 	total := 0
+func contar(hits []golpes.TipoAcaoGolpe, f predicate) int {
+	tamanho := len(hits)
+	total := 0
 
-// 	for i := range tamanho {
-// 		if f(i) {
-// 			total++
-// 		}
-// 	}
+	for i := range tamanho {
+		if f(i) {
+			total++
+		}
+	}
 
-// 	return total
-// }
+	return total
+}
 
-// func ContarAces(hits []TipoAcaoGolpe) int {
-// 	EhAce := func(indice int) bool {
-// 		g := hits[indice]
-// 		return g.Tipo() == HTAce ||
-// 			(g.Tipo() == HTMiss && indice > 0 && hits[indice-1].Tipo() == HTServeIn)
-// 	}
+func ContarAces(hits []golpes.TipoAcaoGolpe) int {
+	EhAce := func(indice int) bool {
+		g := hits[indice]
+		return g.Tipo() == golpes.HTAce ||
+			(g.Tipo() == golpes.HTMiss && indice > 0 && hits[indice-1].Tipo() == golpes.HTServeIn)
+	}
 
-// 	return contar(hits, EhAce)
-// }
+	return contar(hits, EhAce)
+}
 
-// func ContarWinners(hits []TipoAcaoGolpe) int {
-// 	EhWinner := func(indice int) bool {
-// 		g := hits[indice]
-// 		return g.Tipo() == HTAce ||
-// 			(g.Tipo() == HTMiss && indice > 0)
-// 	}
+func ContarWinners(hits []golpes.TipoAcaoGolpe) int {
+	EhWinner := func(indice int) bool {
+		g := hits[indice]
+		return g.Tipo() == golpes.HTAce ||
+			(g.Tipo() == golpes.HTMiss && indice > 0)
+	}
 
-// 	return contar(hits, EhWinner)
-// }
+	return contar(hits, EhWinner)
+}
