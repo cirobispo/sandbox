fd818466d29cf1d1a6c00f4c2d3d926b5b8dd5da
Ciro Bispo <cirobispo@zoho.com>
2026-06-30 14:05:51 -0300

    Funções que farão parte da lista do dados dos jogos. Ainda no inicio.

--- internal/comum/estatisticas/estatisticas.go
+++ internal/comum/estatisticas/estatisticas.go
@@ -1,15 +1,29 @@
 package estatisticas
 
-import "github.com/cirobispo/sandbox/internal/comum/golpes"
+import (
+	"github.com/cirobispo/sandbox/internal/comum/golpes"
+	"github.com/cirobispo/sandbox/internal/comum/jogos/partida"
+)
 
-type predicate func(indice int) bool
+type predicate func(golpes []golpes.TipoAcaoGolpe, indice int) bool
 
-func contar(hits []golpes.TipoAcaoGolpe, f predicate) int {
-	tamanho := len(hits)
+func contarNoSet(set partida.Setting, f predicate) int {
+	total := 0
+	jogos := set.Games()
+	for j := range jogos {
+		ponto := jogos[j].Pontos()[j]
+		total += contar(ponto.Golpes(), f)
+	}
+
+	return total
+}
+
+func contar(golpes []golpes.TipoAcaoGolpe, f predicate) int {
+	tamanho := len(golpes)
 	total := 0
 
 	for i := range tamanho {
-		if f(i) {
+		if f(golpes, i) {
 			total++
 		}
 	}
@@ -17,22 +31,43 @@ func contar(hits []golpes.TipoAcaoGolpe, f predicate) int {
 	return total
 }
 
-func ContarAces(hits []golpes.TipoAcaoGolpe) int {
-	EhAce := func(indice int) bool {
-		g := hits[indice]
+func ContarAces(set partida.Setting) int {
+	EhAce := func(golpes_ []golpes.TipoAcaoGolpe, indice int) bool {
+		g := golpes_[indice]
 		return g.Tipo() == golpes.HTAce ||
-			(g.Tipo() == golpes.HTMiss && indice > 0 && hits[indice-1].Tipo() == golpes.HTServeIn)
+			(g.Tipo() == golpes.HTMiss && indice > 0 && golpes_[indice-1].Tipo() == golpes.HTServeIn)
 	}
 
-	return contar(hits, EhAce)
+	total := contarNoSet(set, EhAce)
+
+	// Jogos := set.Games()
+	// for j := range Jogos {
+	// 	for p := range Jogos[j].Pontos() {
+	// 		ponto := Jogos[j].Pontos()[p]
+	// 		golpes_ := ponto.Golpes()
+	// 		total += contar(golpes_, EhAce)
+	// 	}
+	// }
+
+	return total
 }
 
-func ContarWinners(hits []golpes.TipoAcaoGolpe) int {
-	EhWinner := func(indice int) bool {
-		g := hits[indice]
+func ContarAcess(golpes_ []golpes.TipoAcaoGolpe) int {
+	EhAce := func(golpes_ []golpes.TipoAcaoGolpe, indice int) bool {
+		g := golpes_[indice]
+		return g.Tipo() == golpes.HTAce ||
+			(g.Tipo() == golpes.HTMiss && indice > 0 && golpes_[indice-1].Tipo() == golpes.HTServeIn)
+	}
+
+	return contar(golpes_, EhAce)
+}
+
+func ContarWinners(golpes_ []golpes.TipoAcaoGolpe) int {
+	EhWinner := func(golpes_ []golpes.TipoAcaoGolpe, indice int) bool {
+		g := golpes_[indice]
 		return g.Tipo() == golpes.HTAce ||
 			(g.Tipo() == golpes.HTMiss && indice > 0)
 	}
 
-	return contar(hits, EhWinner)
+	return contar(golpes_, EhWinner)
 }
